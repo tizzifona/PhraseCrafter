4ef8e3599954851299edc0a4659415e43c611580
tizzifona <tizzifona@gmail.com>
2025-06-11 01:47:47 +0200

    feat: added method findPhraseById in PhraseService and PhraseController

--- src/main/java/femcoders/java/phrase_crafter/service/PhraseService.java
+++ src/main/java/femcoders/java/phrase_crafter/service/PhraseService.java
@@ -6,6 +6,7 @@ import femcoders.java.phrase_crafter.repository.PhraseRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PhraseService {
@@ -19,5 +20,9 @@ public class PhraseService {
 
     public List<Phrase> getAllPhrases() {
         return phraseRepository.findAll();
+    }
+
+    public Optional<Phrase> getPhraseById(Long id) {
+        return phraseRepository.findById(id);
     }
 }
