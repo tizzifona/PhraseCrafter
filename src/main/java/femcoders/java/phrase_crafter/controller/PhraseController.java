a5c4ea581f8d4710a1b159dc46386caa37af33a8
tizzifona <tizzifona@gmail.com>
2025-06-11 01:36:24 +0200

    feat: added method getAllPhrases in PhraseService and PhraseController, changed localhost to 8080

--- src/main/java/femcoders/java/phrase_crafter/controller/PhraseController.java
+++ src/main/java/femcoders/java/phrase_crafter/controller/PhraseController.java
@@ -1,4 +1,29 @@
 package femcoders.java.phrase_crafter.controller;
 
+import femcoders.java.phrase_crafter.model.Phrase;
+import femcoders.java.phrase_crafter.service.PhraseService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/phrases")
+@CrossOrigin(origins = "*")
 public class PhraseController {
+
+    private PhraseService phraseService;
+
+    public PhraseController(PhraseService phraseService) {
+        this.phraseService = phraseService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Phrase>> getAllPhrases() {
+        List<Phrase> phrases = phraseService.getAllPhrases();
+        return ResponseEntity.ok(phrases);
+    }
 }
--- src/main/java/femcoders/java/phrase_crafter/service/PhraseService.java
+++ src/main/java/femcoders/java/phrase_crafter/service/PhraseService.java
@@ -1,4 +1,23 @@
 package femcoders.java.phrase_crafter.service;
 
+import femcoders.java.phrase_crafter.model.Phrase;
+import femcoders.java.phrase_crafter.repository.AuthorRepository;
+import femcoders.java.phrase_crafter.repository.PhraseRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PhraseService {
+    private final PhraseRepository phraseRepository;
+    private final AuthorRepository authorRepository;
+
+    public PhraseService(PhraseRepository phraseRepository, AuthorRepository authorRepository) {
+        this.phraseRepository = phraseRepository;
+        this.authorRepository = authorRepository;
+    }
+
+    public List<Phrase> getAllPhrases() {
+        return phraseRepository.findAll();
+    }
 }
