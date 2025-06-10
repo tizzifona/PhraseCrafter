646e76e216ad57b3cc0a615f1f0a1854bb2c077c
tizzifona <tizzifona@gmail.com>
2025-06-10 23:18:04 +0200

    feat: added enum Category

--- src/main/java/femcoders/java/phrase_crafter/model/Category.java
+++ src/main/java/femcoders/java/phrase_crafter/model/Category.java
@@ -1,4 +1,18 @@
 package femcoders.java.phrase_crafter.model;
 
 public enum Category {
+    INSPIRATIONAL("Inspirational"),
+    JOKES("Jokes"),
+    LOREM_IPSUM("Lorem Ipsum");
+
+
+    private final String displayName;
+
+    Category(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
